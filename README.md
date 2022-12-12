9e3a0a406d78f3422aab2e395213158bb668243c
Lincoln Scheer <63661070+BikeLinc@users.noreply.github.com>
2022-12-12 00:05:50 +0000

    Implemented Gradient to ASCII Conversion
    Co-authored-by: Verrone, Lucien <ljverrone@fortlewis.edu>

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-## Instructions for Building ImgToASCII
+# ImgToASCII - The Image to ASCII Art Creator
 
-1) Download repository from https://github.com/BikeLinc/ImgToASCII/
-2) Go into directory. <br>
-  ```cd ImgToASCII```
-3) Compile and build program using makefile. <br>
-  ```make opencv_cmake && make opencv && make program```
-  
-4) If building was sucessfull, run the program using the makefile. <br>
-  ```make program_run```
+Written by Lincoln Scheer and Lucien Verrone
+
+## How to Use
+
+Coming soon...
+
+## Build Instructions
+
+Coming soon...
--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,22 +1,86 @@
+// ImgToASCII
+//
+// main.cpp
+//
+// Lincoln Scheer and Lucien Verrone
+//
+// Compile with:    make program
+// Run with:        make run
+
 #include <stdio.h>
 #include <opencv2/opencv.hpp>
+
 using namespace cv;
-int main(int argc, char** argv )
-{
-    if ( argc != 2 )
-    {
-        printf("usage: DisplayImage.out <Image_Path>\n");
-        return -1;
-    }
-    Mat image;
-    image = imread( argv[1], 1 );
-    if ( !image.data )
-    {
+int main(int argc, char** argv ) {
+
+
+    // Load image with OpenCV
+    Mat image = imread( "res/trollface.jpg", 1 );
+    if ( !image.data ) {
         printf("No image data \n");
         return -1;
+    } else {
+        printf("Image Sucessfully Loaded \n");
+
+        // Try to read through pixel values in matrix
+
+        int imageWidth = image.cols;
+        int imageHeight = image.rows;
+
+
+
+        char asciiMap[imageWidth][imageHeight * 2];
+
+        for (int x = 0; x <= 160; x = x + 2) {
+            for (int y = 0; y <= 240; y++) {
+                int b = image.at<Vec3b>(x, y)[0];//getting the pixel values//
+                int g = image.at<Vec3b>(x, y)[1];//getting the pixel values//
+                int r = image.at<Vec3b>(x, y)[2];//getting the pixel values//
+                //std::cout << "Value of blue channel:" << b << std::endl;//showing the pixel values//
+                //std::cout << "Value of green channel:" << g << std::endl;//showing the pixel values//
+                //std::cout << "Value of red channel:" << r << std::endl;//showing the pixel values//
+
+                unsigned int a = ((b + r + g)/3);
+
+
+                int range = 255/3;
+                char asciiAssigned;
+                if (a < range) {
+
+                    asciiAssigned = '`';
+                }
+                
+                else if (a <= range*2) {
+
+                    asciiAssigned = '^';
+
+                }
+
+                else if (a <= range*3){
+
+                    asciiAssigned = '\\'; // written as '\'
+
+                } 
+                
+                else if (a <= range*4) {
+
+                    asciiAssigned = '_';
+
+                } 
+                
+                else if (a <= range*5) {
+
+                    asciiAssigned = 'a';
+                    
+                }
+
+
+                asciiMap[x][y] = asciiAssigned;
+                asciiMap[x+1][y] = asciiAssigned;
+                std::cout << asciiAssigned;
+            }
+            std::cout << std::endl;
+        }
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
-    imshow("Display Image", image);
-    waitKey(0);
     return 0;
 }
